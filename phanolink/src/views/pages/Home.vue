aeeab334b01eab8e422c8286903a2daa44d77af4
Bach Tien Chat <bachtienchat@gmail.com>
2021-09-29 00:40:06 +0700

    feat ( source ) : create struture folder for project and install bootstrapVue

---
+++ phanolink/src/views/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container">
+      <Header/>
+      <router-view/>
+      <b-button variant="primary">submit</b-button>
+      <Footer/>
+  </div>
+</template>
+
+<script>
+import Header from '../layouts/Header.vue'
+import Footer from '../layouts/Footer.vue'
+
+export default {
+name: 'Home',
+components: {Header,Footer}
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
