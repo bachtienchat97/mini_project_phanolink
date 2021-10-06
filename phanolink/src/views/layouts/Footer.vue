1a030b4962bfe9bd2c7ae8473fa8f16bc5da55ee
Bach Tien Chat <bachtienchat@gmail.com>
2021-10-07 00:50:22 +0700

    feat ( Header ) : create header

--- phanolink/src/views/layouts/Footer.vue
+++ phanolink/src/views/layouts/Footer.vue
@@ -1,5 +1,45 @@
 <template>
-  <h1>Footer</h1>
+  <div class="container">
+    <div class="logo-footer">
+      <img src="../../assets/img/Logo.png" alt="logo-footer">
+      <span class="address">CÔNG TY CỔ PHẦN DƯỢC PHẨM PHANO<br/>Địa chỉ: 31 HỒ BIỂU CHÁNH, P.12, Q.Phú Nhuận, TPHCM</span>
+      <span class="email">
+        MST: 0304860663<br/>Email: cskh@phanolink.com<br/>Tel: 1800 6768
+      </span>
+    </div>
+
+    <div class="support-footer">
+      <h2>HỖ TRỢ KHÁCH HÀNG</h2>
+      <ul>
+        <li><a href="#">Chính sách đổi trả</a></li>
+        <li><a href="#">Chính sách giao hàng</a></li>
+        <li><a href="#">Chính sách bảo mật</a></li>
+        <li><a href="#">Hướng dẫn thanh toán</a></li>
+        <li><a href="#">Hướng dẫn đặt hàng</a></li>
+        <li><a href="#">Giải quyết phản hồi</a></li>
+        <li><a href="#">Giấy phép kinh doanh</a></li>
+      </ul>
+    </div>
+
+    <div class="account-footer">
+      <h2>TÀI KHOẢN</h2>
+      <ul>
+        <li><a href="#">Đăng ký</a></li>
+        <li><a href="#">Đăng nhập</a></li>
+        <li><a href="#">Kiểm tra đơn hàng</a></li>
+      </ul>
+    </div>
+
+    <div class="connect-our">
+      <h2>KẾT NỐI VỚI CHÚNG TÔI</h2>
+      <ul>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+        <li><a href="#"></a></li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +49,7 @@ name: 'Footer'
 </script>
 
 <style lang="scss" scoped>
+.footer {
 
+}
 </style>
