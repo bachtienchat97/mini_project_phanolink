120db43cfadac3ba7a4034bc21eb750665febb82
Bach Tien Chat <bachtienchat@gmail.com>
2021-11-12 16:14:30 +0700

    feat ( manager account ) : create component manager account

--- phanolink/src/views/components/user/Layout.vue
+++ phanolink/src/views/components/user/Layout.vue
@@ -1,13 +1,22 @@
 <template>
-<div>
+<div class="layout-user container">
+  <ManagerAccount />
   <UserInfor />
 </div>
 </template>
 
 <script>
 import UserInfor from './UserInfor';
+import ManagerAccount from '../ManagerAccount';
 
 export default {
-  components: { UserInfor },
+  components: { UserInfor, ManagerAccount },
 };
 </script>
+
+<style lang="scss" scoped>
+.layout-user {
+  display: flex;
+  margin: 20px auto 50px auto;
+}
+</style>
